ed09dd5e76d2bdda8270b53b575c0eb36aaea006
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2025-05-04 04:18:58 +0300

    arg bugfix

--- last_layer_fa.sh
+++ last_layer_fa.sh
@@ -1,6 +1,6 @@
-python run_graph_classification.py --rewiring=none --dataset=reddit --layer_type=GCN --last_layer_fa --device=0 --num_trials=50
-python run_graph_classification.py --rewiring=none --dataset=imdb --layer_type=GCN --last_layer_fa --device=0 --num_trials=50
-python run_graph_classification.py --rewiring=none --dataset=mutag --layer_type=GCN --last_layer_fa --device=0 --num_trials=50
-python run_graph_classification.py --rewiring=none --dataset=enzymes --layer_type=GCN --last_layer_fa --device=0 --num_trials=50
-python run_graph_classification.py --rewiring=none --dataset=proteins --layer_type=GCN --last_layer_fa --device=0 --num_trials=50
-python run_graph_classification.py --rewiring=none --dataset=collab --layer_type=GCN --last_layer_fa --device=0 --num_trials=50
+python run_graph_classification.py --rewiring=none --dataset=reddit --layer_type=GCN --last_layer_fa=True --device=0 --num_trials=50
+python run_graph_classification.py --rewiring=none --dataset=imdb --layer_type=GCN --last_layer_fa=True --device=0 --num_trials=50
+python run_graph_classification.py --rewiring=none --dataset=mutag --layer_type=GCN --last_layer_fa=True --device=0 --num_trials=50
+python run_graph_classification.py --rewiring=none --dataset=enzymes --layer_type=GCN --last_layer_fa=True --device=0 --num_trials=50
+python run_graph_classification.py --rewiring=none --dataset=proteins --layer_type=GCN --last_layer_fa=True --device=0 --num_trials=50
+python run_graph_classification.py --rewiring=none --dataset=collab --layer_type=GCN --last_layer_fa=True --device=0 --num_trials=50
