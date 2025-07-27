ff25bfb52e78fdb827a27a128b00cd25c356ded8
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2025-07-28 01:05:40 +0300

    fixed bug

--- run_node_classification.py
+++ run_node_classification.py
@@ -6,7 +6,7 @@ from experiments.node_classification import Experiment
 import torch
 import numpy as np
 import pandas as pd
-from hyperparams import get_args_from_input, get_args_from_input_node
+from hyperparams import get_args_from_input
 from preprocessing import rewiring, sdrf, fosr, digl
 
 largest_cc = LargestConnectedComponents()
@@ -51,7 +51,7 @@ default_args = AttrDict({
 
 
 results = []
-args = get_args_from_input_node()
+args = get_args_from_input()
 
 if args.dataset:
     # restricts to just the given dataset if this mode is chosen
